da7770664f2adacc848888abf0f19373fef775ab
Rhiali Cândido <rhiali.candido@gmail.com>
2023-03-20 19:36:14 -0400

    fix: Inserted Content Secury Policy html head

--- resources/views/components/layout/guest.blade.php
+++ resources/views/components/layout/guest.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
   <title>@yield('title')</title>
